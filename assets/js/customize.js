2d116de954766c78045be063d4dee6a0fa296a8d
Joseph Pal <josephpal27@gmail.com>
2025-04-05 16:22:53 +0530

    refactor filter section layout; remove unused row elements and improve code organization

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -1,3 +1,4 @@
+// Functionality for Filter Rows Toggle
 let bottomRow = document.querySelectorAll('.filter-body .filter-row .row-center .row-bottom');
 let rowCaret = document.querySelectorAll('.filter-body .filter-row .row-right i');
 
@@ -9,3 +10,15 @@ rowCaret.forEach((caret, index) => {
     }
     );
 });
+
+
+// ----------------------------------------------------------------------------------------
+// Functionality for Clear Search Button
+document.querySelector('.filter-head-right button[type="button"]').addEventListener('click', () => {
+    const checkboxes = document.querySelectorAll('.filter-body .hidden-checkbox');
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = false;
+    });
+});
+
+// ----------------------------------------------------------------------------------------
