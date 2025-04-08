6e9871bcfb663cdacb9e736e3446f41fee5a52e0
Joseph Pal <josephpal27@gmail.com>
2025-04-08 17:34:43 +0530

    Refactor code structure for improved readability and maintainability

---
+++ assets/js/search-results.js
@@ -0,0 +1,19 @@
+let sortBtn = document.querySelector('#sort-btn');
+let sortMenu = document.querySelector('.sort-menu');
+let closeBtn = document.querySelector('.sort-menu .sort-menu-head .fa-xmark');
+
+sortBtn.addEventListener('click', (event) => {
+    event.stopPropagation(); // Prevent click from propagating to the document
+    sortMenu.classList.toggle('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    sortMenu.classList.remove('active');
+});
+
+// Close the sort menu when clicking outside
+document.addEventListener('click', (event) => {
+    if (sortMenu.classList.contains('active') && !sortMenu.contains(event.target)) {
+        sortMenu.classList.remove('active');
+    }
+});
